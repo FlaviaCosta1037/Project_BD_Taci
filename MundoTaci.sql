5c6dd2cade3d44cccce2e292fb33606456949d4e
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-14 16:53:14 -0300

    Adicionado Primary Key nas tabelas FAQ e Client

--- MundoTaci.sql
+++ MundoTaci.sql
@@ -5,8 +5,11 @@ CREATE TABLE FAQ
 (
 FaqID Integer NOT NULL IDENTITY,
 Title Varchar(100),
-Description Varchar (1000)
+Description Varchar (1000),
 );
+
+ALTER TABLE FAQ ADD PRIMARY KEY (FaqID);
+
 CREATE TABLE Client
 (
 AdminID Integer NOT NULL,
@@ -14,3 +17,10 @@ Email Varchar(50),
 Password Varchar(100),
 Name Varchar(50)
 );
+
+ALTER TABLE Client ADD PRIMARY KEY (AdminID);
+
+CREATE TABLE Category
+(
+CategoryID
+)
