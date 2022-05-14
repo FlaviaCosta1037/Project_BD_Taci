3ec29435388532c81ef1232fff00a901d579ff73
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-14 18:18:46 -0300

    Criada tabela Product com suas respectivas chaves estrangeiras

--- MundoTaci.sql
+++ MundoTaci.sql
@@ -53,11 +53,15 @@ CREATE TABLE Manufacturer
 	Name Char (20)
 );
 
+
 CREATE TABLE Product
 (
 	ProductID Integer NOT NULL PRIMARY KEY IDENTITY,
 	Name Varchar (50) NOT NULL,
 	Description Varchar (1000) NOT NULL,
 	Image Varchar (200) NOT NULL,
-	Size_SizeID
+	--Tirar dúvida sobre o atributo Ean13 (char 13) que consta no modelo lógico.
+	ColorID Integer FOREIGN KEY REFERENCES Color,
+	ManufacturerID Integer FOREIGN KEY REFERENCES Manufacturer,
+	SizeID Integer FOREIGN KEY REFERENCES Size
 )
