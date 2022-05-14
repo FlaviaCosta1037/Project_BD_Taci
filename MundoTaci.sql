7ef7e98a85b239c308666b4551e08284f0b898ea
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-14 17:31:13 -0300

    Criada tabela de relacionamento Category and Product

--- MundoTaci.sql
+++ MundoTaci.sql
@@ -3,25 +3,30 @@ GO
 USE MundoTaci;
 CREATE TABLE FAQ 
 (
-FaqID Integer NOT NULL IDENTITY,
-Title Varchar(100),
-Description Varchar (1000),
+	FaqID Integer NOT NULL IDENTITY,
+	Title Varchar(100),
+	Description Varchar (1000),
 );
 
 ALTER TABLE FAQ ADD PRIMARY KEY (FaqID);
 
 CREATE TABLE Client
 (
-AdminID Integer NOT NULL,
-Email Varchar(50),
-Password Varchar(100),
-Name Varchar(50)
+	AdminID Integer NOT NULL,
+	Email Varchar(50),
+	Password Varchar(100),
+	Name Varchar(50)
 );
 
-ALTER TABLE Client ADD PRIMARY KEY (AdminID);
+ALTER TABLE Client ADD PRIMARY KEY (AdminID);clientownclient
 
 CREATE TABLE Category
 (
-CategoryID Integer NOT NULL PRIMARY KEY,
-Name Varchar(50),
+	CategoryID Integer NOT NULL PRIMARY KEY,
+	Name Varchar(50),
+);
+CREATE TABLE OwnCategoryProeduct
+(
+	CategoryID Integer,
+	ProductID Integer
 );
