aaee0ec4d44b628761cc67bde1d74135ec9a54ce
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-14 16:35:53 -0300

    Criada tabela Client

--- MundoTaci.sql
+++ MundoTaci.sql
@@ -7,3 +7,10 @@ FaqID Integer NOT NULL IDENTITY,
 Title Varchar(100),
 Description Varchar (1000)
 );
+CREATE TABLE Client
+(
+AdminID Integer NOT NULL,
+Email Varchar(50),
+Password Varchar(100),
+Name Varchar(50)
+);
