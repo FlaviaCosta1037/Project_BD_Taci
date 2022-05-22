b6e7fe579e20f84c533825adf3f352c915554fba
Flavia Costa <flavinha.goncalves@gmail.com>
2022-05-21 21:13:07 -0300

    Criadas tabelas ProductBonus, StoreProduct, Bonus, Store,SalesMAn

--- MundoTaci.sql
+++ MundoTaci.sql
@@ -65,3 +65,46 @@ CREATE TABLE Product
 	ManufacturerID Integer FOREIGN KEY REFERENCES Manufacturer,
 	SizeID Integer FOREIGN KEY REFERENCES Size
 )
+GO
+CREATE TABLE ProductBonus
+(
+	BonusID INTEGER,
+	ProductID INTEGER
+);
+CREATE TABLE StoreProduct
+(
+	ProductID INTEGER,
+	StoreID INTEGER
+);
+CREATE TABLE Bonus
+(
+	BonusID Integer NOT NULL PRIMARY KEY IDENTITY,
+	Name Varchar(200) NOT NULL,
+	Value smallmoney NOT NULL, 
+	ExpirationDate Date NOT NULL,
+	StoreID Integer FOREIGN KEY REFERENCES Store,
+	SalesManID Integer FOREIGN KEY REFERENCES SalesMan
+);
+CREATE TABLE Store(
+	StoreID INTEGER NOT NULL PRIMARY KEY IDENTITY,
+	Email Varchar(100) NOT NULL,
+	CNPJ Char(14) NOT NULL,
+	Name Varchar(100) NOT NULL,
+	Image Varchar(200) NOT NULL,
+	Adress Varchar(20) NOT NULL,
+	CEP Char(8) NOT NULL,
+	Phone Varchar(20) NOT NULL,
+	Owner Varchar(50) NOT NULL,
+	Password Varchar(200) NOT NULL
+);
+CREATE TABLE SalesMan
+(
+	SalesManID Integer NOT NULL PRIMARY KEY IDENTITY,
+	CPF char(11) NOT NULL,
+	Name Varchar(50) NOT NULL,
+	Phone Varchar(20) NOT NULL,
+	Image Varchar(200) NOT NULL,
+	Email Varchar(100) NOT NULL,
+	Password Varchar(200) NOT NULL,
+	StoreID INTEGER FOREIGN KEY REFERENCES Store
+);
