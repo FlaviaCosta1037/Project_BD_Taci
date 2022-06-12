48ba1b3cc89dc6695f0c7814c3ce898b80753649
Flavia Costa <flavinha.goncalves@gmail.com>
2022-06-12 18:34:40 -0300

    Criada procedure para inserir dados na tabela Store

--- Procedure.sql
+++ Procedure.sql
@@ -43,7 +43,25 @@
 
 --SELECT * FROM Bonus;
 -------------------------------------------------------------------------
+--Procedure para inserção de dados na tabela Store
 
+--SELECT * FROM STORE;
+--CREATE PROCEDURE Store_InserirDados
+--				@Email  VARCHAR(100),
+--				@CNPJ   CHAR(14),
+--				@Name	VARCHAR(100),
+--				@Image	VARCHAR(200),
+--				@Adress	VARCHAR(20),
+--				@CEP	CHAR(8),
+--				@Phone	VARCHAR(20),
+--				@Owner	VARCHAR(50),
+--				@Password VARCHAR(200)
+--AS
+--		INSERT INTO Store VALUES
+--				(@Email, @CNPJ, @Name,@Image, @Adress, @CEP, @Phone, @Owner, @Password);
+
+--EXEC Store_InserirDados 'roupasLindas@uol.com.br', '12456456000488', 'Roupas Lindas', 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+--						'Rua das Fonsecas, 45, Abigobal', '51184452', '8156646325', 'Cecilia', 'funfoutudoA';
 
 
 
