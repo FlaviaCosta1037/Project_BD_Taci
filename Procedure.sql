fe0aa56f5a17299ef6caa0e7320bf350e9ecb2fb
Flavia Costa <flavinha.goncalves@gmail.com>
2022-06-12 17:53:36 -0300

    Criada procedure para inserir dados na tabela Bonus

--- Procedure.sql
+++ Procedure.sql
@@ -1,28 +1,49 @@
+--REGISTRO DE PROCEDURES
+--COMANDOS
+--DROP PROCEDURE <Nome> (Excluir um procedure)
+
 --Procedure para inserir valor em tabela intermediaria 
 
-CREATE PROCEDURE SetTrainingsSalesManValue 
-        @TrainingID INTEGER, 
-        @SalesmanID INTEGER 
-AS 
-BEGIN
-        INSERT INTO TrainingsSalesMan
-            (
-                TrainingID, 
-                SalesmanID
-            )
-        VALUES 
-            (
-            @TrainingID, 
-            @SalesmanID
-            )
-END
+--CREATE PROCEDURE SetTrainingsSalesManValue 
+--        @TrainingID INTEGER, 
+--        @SalesmanID INTEGER 
+--AS 
+--BEGIN
+--        INSERT INTO TrainingsSalesMan
+--            (
+--                TrainingID, 
+--                SalesmanID
+--            )
+--        VALUES 
+--            (
+--            @TrainingID, 
+--            @SalesmanID
+--            )
+--END
 
-EXEC SetTrainingsSalesManValue
-    @TrainingID = 4,
-    @SalesmanID = 1
+--EXEC SetTrainingsSalesManValue
+--    @TrainingID = 4,
+--    @SalesmanID = 1
 
-SELECT * FROM TrainingsSalesMan;
-SELECT * FROM SalesMan;
---SELECT * FROM Trainings;
+-----------------------------------------------------------------------
 
---Observações Professor:
+--Procedure para inserção de dados na tabela Bonus
+
+--CREATE PROCEDURE Bonus_InserirDados
+--				@Name VARCHAR(200),
+--				@Value SMALLMONEY,
+--				@ExpirationDate DATE,
+--				@StoreID INTEGER,
+--				@SalesmanID INTEGER
+--AS
+--		INSERT INTO Bonus VALUES
+--				(@Name, @Value, @ExpirationDate, @StoreID, @SalesmanID);
+
+--EXEC Bonus_InserirDados 'Vale Felicidade', 200, '2022-09-02',NULL, NULL
+
+--SELECT * FROM Bonus;
+-------------------------------------------------------------------------
+
+
+
+
