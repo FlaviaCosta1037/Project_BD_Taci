ac5f53cdd4cf5bdca5f718a50acba3f8ebaaceb9
FlaviaCo <flavinha.goncalves@gmail.com>
2022-06-14 16:57:50 -0300

    Ajuste nas tabelas interligadas

--- Interligar Tabelas.sql
+++ Interligar Tabelas.sql
@@ -8,19 +8,21 @@
 --UPDATE product set ColorID = 4, ManufacturerID = 3, SizeID = 3 where ProductID = 2;
 --UPDATE product set ColorID = 3, ManufacturerID = 1, SizeID = 2 where ProductID = 3;
 
---Select p.Name, p.Description, p.Image, c.Name, m.Name, s.SizeID from product AS p
+--Select p.Name, p.Description, p.Image, c.Name as Cor, m.Name as Tipo, s.SizeID as Tamanho from product AS p
 --JOIN color AS c ON p.ColorID = c.ColorID 
 --JOIN manufacturer AS m ON p.ManufacturerID = m.ManufacturerID
 --JOIN size AS s ON p.SizeID = s.SizeID;
 
---select * from product;
+
 ---------------------------------------------------------------------------------------
 
 --Requisitos: Criar uma consulta interligando as tabelas Trainings com Type
 
-select Trainings.Title, Trainings.Description, Trainings.TotalViews, Trainings.StoreViews, Trainings.Video, Trainings.UniqueUserViews, Type.Name
-from Type
-join Trainings on Trainings.TrainingID = type.TypeID;
+--select Trainings.Title, Trainings.Description, Trainings.TotalViews, Trainings.StoreViews, Trainings.Video, Trainings.UniqueUserViews, Type.Name as Tipo
+--from Type
+--join Trainings on Trainings.TrainingID = type.TypeID;
 
 
-SELECT * FROM Trainings;
+---------------------------------------------------------------------------------------
+
+
