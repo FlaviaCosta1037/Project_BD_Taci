e14c246fb4f4533b35d5feac5dc817eaf0812ea9
Flavia Costa <flavinha.goncalves@gmail.com>
2022-06-18 09:04:20 -0300

    Criada procedure para encontrar o cadastro do cliente pelo cnpj

--- Procedure.sql
+++ Procedure.sql
@@ -25,67 +25,23 @@ EXEC SetTrainingsSalesManValue
     @TrainingID = 2,
     @SalesmanID = 1;
 
+
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- CRIA A STORED PROCEDURE
+-- BUSCA DO CADASTRO DO CLIENTE PELO CNPJ
 
-----Procedure para inserção de dados na tabela Bonus
+CREATE PROCEDURE cadastroCNPJ_SELECT
 
-CREATE PROCEDURE Bonus_InserirDados
-				@Name VARCHAR(200),
-				@Value SMALLMONEY,
-				@ExpirationDate DATE,
-				@StoreID INTEGER,
-				@SalesmanID INTEGER
+@CNPJ CHAR(14)
 AS
-		INSERT INTO Bonus VALUES
-				(@Name, @Value, @ExpirationDate, @StoreID, @SalesmanID);
+-- COMANDO T-SQL PARA OBTER RESULTADO
+SELECT * FROM Store
+       WHERE CNPJ = @CNPJ;
 
-EXEC Bonus_InserirDados 'Vale Felicidade', 200, '2022-09-02',NULL, NULL
+EXECUTE	cadastroCNPJ_SELECT 14017797000109;
 
-SELECT * FROM Bonus;
--------------------------------------------------------------------------------------------------------------------------------------------------------------
-----Procedure para inserção de dados na tabela Store
+SELECT * FROM Store;
 
-SELECT * FROM STORE;
-CREATE PROCEDURE Store_InserirDados
-				@Email  VARCHAR(100),
-				@CNPJ   CHAR(14),
-				@Name	VARCHAR(100),
-				@Image	VARCHAR(200),
-				@Adress	VARCHAR(20),
-				@CEP	CHAR(8),
-				@Phone	VARCHAR(20),
-				@Owner	VARCHAR(50),
-				@Password VARCHAR(200)
-AS
-		INSERT INTO Store VALUES
-				(@Email, @CNPJ, @Name,@Image, @Adress, @CEP, @Phone, @Owner, @Password);
-
-EXEC Store_InserirDados 'roupasLindas@uol.com.br', '12456456000488', 'Roupas Lindas', 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
-						'Rua das Fonsecas, 45, Abigobal', '51184452', '8156646325', 'Cecilia', 'funfoutudoA';
-
--------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-----Procedure para inserção de dados na tabela Client
-SELECT * FROM Client;
-CREATE PROCEDURE Cliente_InserirDadosCliente
-				@Email		VARCHAR(50),
-				@Password	VARCHAR(100),
-				@Name		VARCHAR(50)
-AS
-		INSERT INTO Client VALUES
-				(@Email, @Password, @Name);
-
-EXEC Cliente_InserirDadosCliente 'patriciosilva@gmail.com', 'patopatosdsd', 'Patricio Silva';
--------------------------------------------------------------------------------------------------------------------------------------------------------------
-----Procedure para inserção de dados na tabela Color
-SELECT * FROM Color;
-CREATE PROCEDURE Color_InserirDadosCliente
-				@Name VARCHAR (20)
-AS
-		INSERT INTO Color VALUES (@Name);
-				
-EXEC Color_InserirDadosCliente 'Laranja';
--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
